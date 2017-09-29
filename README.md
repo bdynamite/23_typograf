a93021e5cf336fbb91cbec6d8e33d6f4786b7acf
bdynamite <bdynamite@yandex.ru>
2017-09-29 16:45:13 +0300

    first try

--- README.md
+++ README.md
@@ -1,6 +1,27 @@
 # Typograf Service
 
-{TODO. There will be project description}
+This script fixes your typograf errors
+
+# How to Install
+
+Python 3 should be already installed. Then use pip (or pip3 if there is a conflict with old Python 2 setup) to install dependencies:
+
+```bash
+pip install -r requirements.txt # alternatively try pip3
+```
+
+# Quickstart
+
+
+Example of script launch on Windows, Python 3.5:
+
+```#!bash
+
+$ python server.py
+
+```
+
+then open [http://127.0.0.1:5000/](http://127.0.0.1:5000/)
 
 # Project Goals
 
--- server.py
+++ server.py
@@ -1,10 +1,30 @@
-from flask import Flask, render_template
+import re
+
+from flask import Flask, render_template, request
 
 app = Flask(__name__)
 
-@app.route('/')
+
+@app.route('/', methods=['GET', 'POST'])
 def form():
-    return render_template('form.html')
+    if request.method == 'GET':
+        return render_template('form.html')
+    else:
+        text = request.form['text']
+        return render_template('form.html', text=text, result=typograf(text))
+
+
+def typograf(text):
+    nbsp = '\u00A0'
+    text = re.sub(r'''(['"])(.*?)(\1)''', r'«\2»', text)
+    text = re.sub(r' - ', r' — ', text)
+    text = re.sub(r'(?<=\d)(-)(?=\d)', r'‒', text)
+    text = re.sub(r'(?<=\d)(\s)(?=[\D])', nbsp, text)
+    text = re.sub(r' {2,}', r' ', text)
+    text = re.sub(r'\r\n{2,}', r'\r\n', text)
+    text = re.sub(r'(?<=(?<=\s[а-яА-Я]{2})|(?<=\s[а-яА-Я]{1}))(\s)(?=.)', nbsp, text)
+    return text
+
 
 if __name__ == "__main__":
     app.run()
